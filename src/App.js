51754de4c9f112e13b4af11148b5b86f09b5c69d
Brenda <brendamvreal@gmail.com>
2022-05-27 18:12:24 -0300

    Add dark styling.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+// import logo from './logo.svg';
 import './App.css';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
-// import { useState } from "react";
+import { useState } from "react";
 
 function App() {
+  const [light, setLight] = useState("on");
+  const dark = (light === 'off') ? 'dark' : '';
+  const switchLight = () => {
+    if(light === "on") {
+      setLight("off")
+    } else {
+      setLight("on")
+    }
+  }
+
+
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} switchLight={switchLight} />
         <TextRepeaterButton />
       </section>
+
     </div>
   );
 }
